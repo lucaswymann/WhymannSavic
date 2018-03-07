53eb65bdd2f5a0c1f6a6ea3340e9526ac8ed49d5
lucaswymann <31436898+lucaswymann@users.noreply.github.com>
2018-03-07 09:24:17 +0100

    login

---
+++ Loginn/app/src/main/java/com/example/lucaswy/loginn/MainActivity.java
@@ -0,0 +1,83 @@
+package com.example.lucaswy.loginn;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static EditText username;
+    private static EditText password;
+    private static TextView attempt;
+    private static Button login_button;
+    int attempt_counter = 5;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        LoginButton();
+    }
+
+    public void LoginButton() {
+        username = (EditText) findViewById(R.id.editText_user);
+        password = (EditText) findViewById(R.id.editText_password);
+        attempt = (TextView) findViewById(R.id.textView_attempt);
+        login_button = (Button) findViewById(R.id.button_login);
+
+        attempt.setText(Integer.toString(attempt_counter));
+
+        login_button.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if (username.getText().toString().equals("user")&&
+                            password.getText().toString().equals("pass"))
+                        {
+                            Toast.makeText(MainActivity.this, "Username and password is correct",
+                                    Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent("com.abhinavhackpundit.loginapp.User");
+                            startActivity(intent);
+
+                        }
+                        else{
+                            Toast.makeText(MainActivity.this, "Username and password is NOT correct",
+                                    Toast.LENGTH_SHORT).show();
+                            attempt_counter--;
+                            attempt.setText(Integer.toString(attempt_counter));
+                            if (attempt_counter == 0)
+                                login_button.setEnabled(false);
+                        }
+                    }
+                }
+        );
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_login, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
